dae7db149d5bfcf2b1eacb30c103edea33137580
Martin Pruus <144675657+MartinP69@users.noreply.github.com>
2023-10-02 17:30:11 +0300

    Add files via upload

---
+++ checkTemp.js
@@ -0,0 +1,19 @@
+//Ülesanne checkTemp - 2.Okt 2023
+
+temperatuur = []
+temp = prompt('Kirjuta siia katla temperatuur 1-60.')
+temperatuur.push[temp]
+
+function checkTemp(temp) {
+if (temp >= 0 && temp <= 20) {
+    return -1;
+}
+else if (temp >= 21 && temp <= 40) {
+    console.log('0')
+}
+else if (temp >= 41 && temp <= 60) {
+    console.log('1')
+}
+}
+
+
