c9259e628dfc299cd9aa4a3b68b107746e323c43
Martin Pruus <144675657+MartinP69@users.noreply.github.com>
2023-10-03 18:09:24 +0300

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -2,18 +2,33 @@
 
 temperatuur = []
 temp = prompt('Kirjuta siia katla temperatuur 1-60.')
-temperatuur.push[temp]
+temperatuur.push(temp)
 
 function checkTemp(temp) {
 if (temp >= 0 && temp <= 20) {
-    return -1;
+return -1;
 }
 else if (temp >= 21 && temp <= 40) {
-    console.log('0')
+return 0;
 }
 else if (temp >= 41 && temp <= 60) {
-    console.log('1')
+return 1;
 }
 }
+
+var tulemus = checkTemp(temperatuur);
+
+if (tulemus === -1) {
+    console.log('Katel on liiga külm')
+}
+else if (tulemus === 0) {
+    console.log('Katel on parasjagu soe')
+}
+else if (tulemus === 1) {
+    console.log('Katel on liiga kuum')
+}
+
+
+
 
 
